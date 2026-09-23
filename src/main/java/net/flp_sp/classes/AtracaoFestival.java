c06e0b79119ac84d7fd3c3e7a81878487f55547c
Felipe Sampaio <o22466244@gmail.com>
2026-09-23 14:08:13 -0300

    add the basic structure for AtracaoFestival class

--- src/main/java/net/flp_sp/classes/AtracaoFestival.java
+++ src/main/java/net/flp_sp/classes/AtracaoFestival.java
@@ -1,5 +1,39 @@
 package net.flp_sp.classes;
 
 public class AtracaoFestival {
+    private String nome;
+    private String hora;
+    private double cache;
+    private double valorIngresso;
+    private int capacidadePublico;
+    private int ingressosVendidos;
 
+    public AtracaoFestival(String nome, String hora, double cache, double valorIngresso, int capacidadePublico) {
+        this.ingressosVendidos = 0;
+        this.nome = nome;
+        this.hora = hora;
+        this.cache = cache;
+        this.valorIngresso = valorIngresso;
+        this.capacidadePublico = capacidadePublico;
+    }
+
+    /* Verifica se existem vagas suficientes pra quantidade de ingressos que desejam comprar
+    e incrementam a quantidade passada aos ingressos vendidos*/
+    public boolean venderIngresso(int quantidade){
+        if (quantidade <= getIngressosDisponiveis()){
+            ingressosVendidos += quantidade;
+            return true;
+        }
+        return false;
+    }
+
+    // Retorna a quantidade de ingressos disponiveis
+    public int getIngressosDisponiveis(){
+        return capacidadePublico - ingressosVendidos;
+    }
+
+    // Retorna a receita total
+    public double calcularReceita(){
+        return valorIngresso * ingressosVendidos;
+    }
 }
