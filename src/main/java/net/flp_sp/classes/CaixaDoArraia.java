97e3f3c249e1dcdf166b14760999710080aee24b
Felipe Sampaio <o22466244@gmail.com>
2026-09-23 14:22:12 -0300

    made basic structure for CaixaDoArraia class

--- src/main/java/net/flp_sp/classes/CaixaDoArraia.java
+++ src/main/java/net/flp_sp/classes/CaixaDoArraia.java
@@ -1,5 +1,15 @@
 package net.flp_sp.classes;
 
 public class CaixaDoArraia {
+    private static double saldoGeral;
+    private static final double TAXA_SERVICO = 0.05;
 
+    public void registrarVenda(double valor){
+        valor = (valor * (1 + TAXA_SERVICO));
+        saldoGeral += valor;
+    }
+
+    public static double consultarSaldoGeral(){
+        return saldoGeral;
+    }
 }
