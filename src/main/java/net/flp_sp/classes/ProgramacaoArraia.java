9c6b7691b808cf6a5bbf61b31bd7f3d6e3f94f13
Felipe Sampaio <o22466244@gmail.com>
2026-09-23 14:36:08 -0300

    create the structure for ProgramacaoArraia class

--- src/main/java/net/flp_sp/classes/ProgramacaoArraia.java
+++ src/main/java/net/flp_sp/classes/ProgramacaoArraia.java
@@ -1,5 +1,32 @@
 package net.flp_sp.classes;
 
 public class ProgramacaoArraia {
+    private String[] atracoes;
+    private int quantidadeAtual;
+    private final int MAX_ATRACOES;
 
+    public ProgramacaoArraia() {
+        MAX_ATRACOES = 10;
+        this.atracoes = new String[MAX_ATRACOES];
+        this.quantidadeAtual = 0;
+    }
+
+    public void adicionarAtracao(AtracaoFestival atracao){
+        if (quantidadeAtual < MAX_ATRACOES){
+            this.atracoes[quantidadeAtual] = // atracao.getname;
+            quantidadeAtual++;
+        }
+    }
+
+    public void listarProgramacao(){
+        // Acionar metodo de imprimir atracao na classe AtracaoFestival para todos os itens
+    }
+
+    public void buscarPorNome(String nome){
+        for (int i = 0; i < quantidadeAtual; i++){
+            if (// atracao.getname == nome){
+                return // atracao;    
+            }
+        }
+    }
 }
